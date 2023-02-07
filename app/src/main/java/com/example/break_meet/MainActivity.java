de946522eb5cff6dbb0719760dc5a29a15919e2a
Salah Tawafsha <93351227+SalahTawafsha@users.noreply.github.com>
2023-02-08 01:08:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/break_meet/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.break_meet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore fireStore = FirebaseFirestore.getInstance();
+
+    Map<String, String> map = new HashMap<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fireStore.collection("")
+                .document("").set(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+}
