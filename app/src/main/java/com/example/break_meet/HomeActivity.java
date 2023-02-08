cfcffda86b6ccddfd5c773a130366c452022e12e
Salah Tawafsha <93351227+SalahTawafsha@users.noreply.github.com>
2023-02-08 16:50:20 +0200

    update

--- app/src/main/java/com/example/break_meet/HomeActivity.java
+++ app/src/main/java/com/example/break_meet/HomeActivity.java
@@ -3,6 +3,8 @@ package com.example.break_meet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void add(View view) {
+    }
+
+    public void select(View view) {
+    }
+
+    public void myMeetings(View view) {
+    }
+
+    public void updateProfile(View view) {
     }
 }
---
+++ app/src/main/java/com/example/break_meet/SelectMeetingActivity.java
@@ -0,0 +1,14 @@
+package com.example.break_meet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectMeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_meeting);
+    }
+}
