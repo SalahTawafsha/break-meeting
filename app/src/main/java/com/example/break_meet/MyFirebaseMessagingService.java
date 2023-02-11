1fbbd2aabc615bad00e4f5a51c70957d08deb6ce
Salah Tawafsha <93351227+SalahTawafsha@users.noreply.github.com>
2023-02-11 12:05:29 +0200

    update

--- app/src/main/java/com/example/break_meet/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/break_meet/MyFirebaseMessagingService.java
@@ -24,7 +24,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Intent intent = new Intent(this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelID)
                 .setAutoCancel(true)
@@ -48,8 +48,27 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
         if(message.getNotification() != null){
-            generateNotification(message.getNotification().getTitle(), message.getNotification().getBody());
+            generateNotification(message.getNotification().getTitle(),message.getNotification().getBody());
         }
+//        String title = message.getNotification().getTitle();
+//        String text = message.getNotification().getBody();
+//
+//        final String CHANEL_ID = "fast_notification";
+//
+//        NotificationChannel channel = new NotificationChannel(CHANEL_ID
+//                , "FAST_NOTIFICATION"
+//                , NotificationManager.IMPORTANCE_HIGH);
+//
+//        getSystemService(NotificationManager.class).createNotificationChannel(channel);
+//
+//        Notification.Builder notification = new Notification.Builder(this, CHANEL_ID)
+//                .setAutoCancel(true)
+//                .setContentTitle(title)
+//                .setContentText(text);
+//
+//        NotificationManagerCompat.from(this).notify(1, notification.build());
+//
+//        super.onMessageReceived(message);
 
     }
 
